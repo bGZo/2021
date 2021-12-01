9a0afb6345bf9e8b10f7dce1b60773b4eb88602c
bGZoCg <bgzocg@outlook.com>
2021-12-01 22:53:10 +0800

    :pencil: add gitignore, add config, add action file

---
+++ issueDashboard.py
@@ -0,0 +1,30 @@
+#! /usr/bin/python3
+from github import Github
+from cfig import GithubToken
+from config import (
+    IssueTableHead,
+    IssueTableTemplate
+)
+
+issueFileName = "index.md"
+
+g = Github(GithubToken)
+me = g.get_user().login
+
+repo = g.get_repo("bgzocg/2021")
+openIssues = repo.get_issues(state='open', creator=me, sort='updated')
+
+
+with open(issueFileName, "w+") as f:
+    f.write( IssueTableHead )
+
+    for issue in openIssues:
+        issueName = str(issue.title)
+        issueUpdate = str(issue.updated_at)
+        issueUrl = '#'+ str(issue.number)
+
+        f.write( IssueTableTemplate.format(
+            issueName = issueName, 
+            issueUpdate = issueUpdate, 
+            issueUrl = issueUrl
+        ) )
