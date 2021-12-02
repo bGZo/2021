b0f4015d6606a2dd0882bc115feb46948ff836e8
bGZoCg <bgzocg@outlook.com>
2021-12-02 14:44:24 +0800

    :bug: found bug, haha ;_;

--- issueDashboard.py
+++ issueDashboard.py
@@ -4,36 +4,29 @@ from config import (
     IssueTableHead,
     IssueTableTemplate
 )
-import argparse
-
-# import sys
-
-parser = argparse.ArgumentParser()
-parser.add_argument("github_token", help="github_token")
-options = parser.parse_args()
-GithubToken = options.github_token
+import sys
 
 GithubToken = sys.argv[1]
 print(GithubToken)
 
-# issueFileName = "index.md"
+issueFileName = "index.md"
 
-# g = Github(GithubToken)
-# me = g.get_user().login
+g = Github(GithubToken)
+me = g.get_user().login
 
-# repo = g.get_repo("bgzocg/2021")
-# openIssues = repo.get_issues(state='open', sort='updated') # creator=me,
+repo = g.get_repo("bgzocg/2021")
+openIssues = repo.get_issues(state='open', sort='updated') # creator=me,
 
 
-# with open(issueFileName, "w+") as f:
-#     f.write( IssueTableHead )
-#     for issue in openIssues:
-#         issueName = str(issue.title)
-#         issueUpdate = str(issue.updated_at)
-#         issueUrl = '#'+ str(issue.number)
+with open(issueFileName, "w+") as f:
+    f.write( IssueTableHead )
+    for issue in openIssues:
+        issueName = str(issue.title)
+        issueUpdate = str(issue.updated_at)
+        issueUrl = '#'+ str(issue.number)
 
-#         f.write( IssueTableTemplate.format(
-#             issueName = issueName, 
-#             issueUpdate = issueUpdate, 
-#             issueUrl = issueUrl
-#         ) )
+        f.write( IssueTableTemplate.format(
+            issueName = issueName, 
+            issueUpdate = issueUpdate, 
+            issueUrl = issueUrl
+        ) )
